cdffa8a13a02872ef82521069df896a961d76b17
HarryPotter0107 <bhanuupadhyay87@gamil.com>
2023-10-02 14:37:58 +0530

    encap assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
-public class RWOnly {
-    
+public class RWOnly
+{
+    private String name;
+
+    public RWOnly() {
+        this.name = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
